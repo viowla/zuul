4a021fc4d1139b12da8a61733825ef1eff4114ca
viowla <55015538+viowla@users.noreply.github.com>
2020-11-15 20:20:31 +0600

    refactor imports

--- src/main/java/kz/iitu/zuul/JwtTokenAuthenticationFilter.java
+++ src/main/java/kz/iitu/zuul/JwtTokenAuthenticationFilter.java
@@ -8,7 +8,7 @@ import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import kz.iitu.authservice.services.JwtConfig;
+import kz.iitu.commonservice.services.JwtConfig;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
